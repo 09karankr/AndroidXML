fcae1e6ebf0bfb40fc471ab6fcabc59ac325b578
Karan Kumar <officialkaran39@gmail.com>
2024-01-08 04:18:21 +0530

    complete the sign up page

--- Ui Ux/app/src/main/java/com/example/uiux/SignUp.kt
+++ Ui Ux/app/src/main/java/com/example/uiux/SignUp.kt
@@ -3,11 +3,9 @@ package com.example.uiux
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_sign_up)
     }
 }
