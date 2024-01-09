77f66175a9546745ee9d401777aa23d438270ff0
Karan Kumar <officialkaran39@gmail.com>
2024-01-10 03:06:50 +0530

    complete sign up page

--- database/app/src/main/java/com/example/database16/MainActivity.kt
+++ database/app/src/main/java/com/example/database16/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.database16
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
