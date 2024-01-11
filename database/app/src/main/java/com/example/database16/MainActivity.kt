0f1834aad15abc30cb731bf65d9f48c92ea3a9da
Karan Kumar <officialkaran39@gmail.com>
2024-01-11 23:12:25 +0530

    Connection established to firebase

--- database/app/src/main/java/com/example/database16/MainActivity.kt
+++ database/app/src/main/java/com/example/database16/MainActivity.kt
@@ -4,12 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var database : DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val signButton = findViewById<Button>(R.id.btnSignUp)
+        val etName = findViewById<TextInputEditText>(R.id.etName)
+        val etMail = findViewById<TextInputEditText>(R.id.etMail)
+        val userID = findViewById<TextInputEditText>(R.id.etUserName)
+        val userPassword = findViewById<TextInputEditText>(R.id.etPassword)
+
+
+        signButton.setOnClickListener {
+            val mail = etMail.text.toString()
+            val name = etName.text.toString()
+            val password = userPassword.text.toString()
+            val uniqueId = userID.text.toString()
+
+
+            val user = User(name, mail, password, uniqueId)
+            database = FirebaseDatabase.getInstance().getReference("User")
+            database.child(uniqueId).setValue(user).addOnSuccessListener {
+                etName.text?.clear()
+                Toast.makeText(this, "User Registered", Toast.LENGTH_SHORT).show()
+            }.addOnSuccessListener {
+                Toast.makeText(this,"Failed",Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
---
+++ database/app/src/main/java/com/example/database16/User.kt
@@ -0,0 +1,11 @@
+package com.example.database16
+
+data class User(
+    val email : String,
+    val name : String,
+
+    val password : String,
+    val uniqueId : String
+){
+
+}
