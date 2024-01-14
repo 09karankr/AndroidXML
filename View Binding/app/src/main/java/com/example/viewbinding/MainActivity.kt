df5a23746fc1b2ef01ec967c3b8ca3f32002162e
Karan Kumar <officialkaran39@gmail.com>
2024-01-15 00:37:22 +0530

    completed the view binding

--- View Binding/app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ View Binding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.viewbinding
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            if (binding.checkBox.isChecked){
+
+            }else{
+                binding.checkBox.buttonTintList = ColorStateList.valueOf(Color.RED)
+                Toast.makeText(this,"Please accept the T&C", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
