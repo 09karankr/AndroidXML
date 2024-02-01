4c2a07a227d7d77be4b2901f84b0a0ada30a825b
Karan Kumar <officialkaran39@gmail.com>
2024-02-01 15:21:45 +0530

    added the dependency for circle image view

---
+++ ListViewProject2/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ListViewProject2"
+include(":app")
+ 
