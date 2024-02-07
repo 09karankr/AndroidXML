a4a02e41c79eefd07d59d784dd20a3cc020d2a19
Karan Kumar <officialkaran39@gmail.com>
2024-02-07 09:04:36 +0530

    done each row

---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var myRecyclerView : RecyclerView
+    lateinit var newsArrayList: ArrayList<News>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myRecyclerView = findViewById(R.id.recyclerView)
+
+        var newsImageArray = arrayOf(
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6
+        )
+        var newsHeadingArray = arrayOf(
+            "U.K Foreign Secretary james cleverly raises issue of bbc tax searches with eam jaishankar",
+            "cooking gas prices hiked by 50 for domestic, 350 for commercial cylinders",
+            "joe biden appoints two prominent indian american corporate leaders to his export council",
+            "sergey lavrov will raise suspected bombing of nord stream ii at G20: Russian foreign ministry",
+            "belarusian leader lukashenko bisits china amid ukraine tensions",
+            "china rips new U.s house committee on countering beijing",
+            "largest gathering of foreign ministers hosted by any g20 presidency foreign secretary vinay kwatra"
+
+       )
+    }
+}
---
+++ RecyclerView/app/src/main/java/com/example/recyclerview/News.kt
@@ -0,0 +1,3 @@
+package com.example.recyclerview
+
+data class News(var newsHeading : String, var newsImage : Int)
