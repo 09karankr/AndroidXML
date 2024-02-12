7fdbb78faadc0dc7f5914e09f54affaef8effd6e
Karan Kumar <officialkaran39@gmail.com>
2024-02-12 14:13:36 +0530

    completed the notification part

---
+++ Notifications/app/src/main/java/com/example/notifications/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.notifications
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID = "channelId"
+    private val CHANNEL_NAME ="ChannelName"
+    private val notificationId = 0
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+        val notification = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setContentTitle("App development roadmap")
+            .setContentText("Congratulations for showing up today")
+            .setSmallIcon(R.drawable.baseline_accessibility_new_24)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(this)
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+           notificationManager.notify(notificationId,notification)
+        }
+
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "this is my notification channel"
+            }
+
+            val managers = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            managers.createNotificationChannel(channel)
+        }
+    }
+}
