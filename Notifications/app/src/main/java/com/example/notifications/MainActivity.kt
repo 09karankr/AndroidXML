7fc4c250c50dfaa54a723d48c9435751a14beaf9
Karan Kumar <officialkaran39@gmail.com>
2024-02-12 14:27:00 +0530

    done Pending intent

--- Notifications/app/src/main/java/com/example/notifications/MainActivity.kt
+++ Notifications/app/src/main/java/com/example/notifications/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.notifications
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -24,10 +26,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         createNotificationChannel()
+
+        // Pending intent
+        val intent = Intent(this,MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_MUTABLE)
+
+
         val notification = NotificationCompat.Builder(this,CHANNEL_ID)
             .setContentTitle("App development roadmap")
             .setContentText("Congratulations for showing up today")
             .setSmallIcon(R.drawable.baseline_accessibility_new_24)
+            .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .build()
 
